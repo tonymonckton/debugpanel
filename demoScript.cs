e431507550340b4df5163d68885c9788302a4050
Tony Monckton <tony@tonymonckton.co.uk>
2019-11-27 11:40:02 +0000

    Update demoScript.cs

--- demoScript.cs
+++ demoScript.cs
@@ -16,19 +16,6 @@ public class demoScript : MonoBehaviour
         debugLog.instance.Log("Input", "Delta Time", Time.deltaTime);
         debugLog.instance.Log("System", "Time", Time.time);
         debugLog.instance.Log("Input", "hello", "hello");
-        debugLog.instance.Log("Objects", "sphere", this.transform.position);
-
-/*
-        CategoryLog myCat = new CategoryLog();
-        myCat.category = "test1";
-        debugLog.instance.addCategory( myCat );
-
-        CategoryLog cat = debugLog.instance.getCategory( "test1" );
-
-        if ( cat != null ) {
-            cat.expanded = true;
-            debugLog.instance.setCategory(cat);
-        }
-*/    
+        debugLog.instance.Log("Objects", "sphere", this.transform.position);   
     }
 }
