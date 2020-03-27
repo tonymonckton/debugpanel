5348a04b03910a66705caf0d582b21475b2c026a
TonyMonckton <tony@tonymonckton.co.uk>
2020-03-27 12:20:57 +0000

    add onscreen debugger

---
+++ debugOnScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using TM.Utils;
+
+public class debugOnScreen : MonoBehaviour
+{
+    private string newline = "\r\n"; 
+    public  Text textPanel;
+
+    void Update()
+    {
+        if (textPanel != null) {
+            string message = "";
+            textPanel.text = "";
+
+            Dictionary<string, MessageLog>msgs =  debugLog.instance.messageLog;
+            var ordered = msgs.OrderBy(x => x.Value.category);
+            string cat = "";
+            foreach(KeyValuePair<string, MessageLog> entry in ordered)
+            {
+                if ( cat != entry.Value.category) {
+                    cat = entry.Value.category;
+                    message += ("<b>"+cat+"</b>"+newline);
+                }
+
+                string msg = "    " + entry.Value.name + ": " + entry.Value.text;
+                message += (msg+newline);
+            }
+
+            textPanel.text = message;
+        }
+    }
+}
